d32f7a124d801537cdc47bebafaefffb31ed646c
Sohit Pal <sohitpal@outlook.com>
2017-11-19 19:53:21 -0500

    Added hamburger menu

--- app/OnYourMark/app/src/main/java/com/example/mikezurawski/onyourmark/views/MainActivity.java
+++ app/OnYourMark/app/src/main/java/com/example/mikezurawski/onyourmark/views/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.mikezurawski.onyourmark.views;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mikezurawski.onyourmark.R;
 
 import com.example.mikezurawski.onyourmark.database.BudgetItem;
 import com.example.mikezurawski.onyourmark.database.DatabaseHandler;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.util.Date;
 
@@ -22,13 +33,15 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    Drawer drawer = null;
     DatabaseHandler database = null;
-    TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initHamburgerMenu(this);
 
         // ?
         database = new DatabaseHandler(this);
@@ -74,10 +87,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        textView = (TextView) findViewById(R.id.app_title);
     }
 
+    // TODO: Remove -- Debugging only
     private void displayAllData() {
         String toPrint = "";
         for (BudgetItem item : database.getBudgetItems()) {
@@ -88,5 +100,92 @@ public class MainActivity extends AppCompatActivity {
                     + " |\n";
         }
         System.out.println(toPrint);
+    }
+
+    private void initHamburgerMenu(final Activity activity) {
+
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withName("Home")
+                .withIcon(FontAwesome.Icon.faw_home)
+                .withIconTintingEnabled(true)
+                .withSelectable(false)
+                .withSetSelected(false);
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem()
+                .withIdentifier(2)
+                .withName("Add New Item")
+                .withIcon(FontAwesome.Icon.faw_camera)
+                .withIconTintingEnabled(true)
+                .withSelectable(false)
+                .withSetSelected(false);
+        PrimaryDrawerItem item3 = new PrimaryDrawerItem()
+                .withIdentifier(3)
+                .withName("Settings")
+                .withIcon(FontAwesome.Icon.faw_sliders)
+                .withIconTintingEnabled(true)
+                .withSelectable(false)
+                .withSetSelected(false);
+        PrimaryDrawerItem item4 = new PrimaryDrawerItem()
+                .withIdentifier(10)
+                .withName("About Us")
+                .withIcon(R.drawable.avd_hide_password_1)
+                .withIconTintingEnabled(true)
+                .withSelectable(false)
+                .withSetSelected(false);
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(activity)
+                .withHeaderBackground(R.color.md_black_1000)
+                .addProfiles(new ProfileDrawerItem()
+                        .withIcon(R.mipmap.ic_launcher)
+                        .withName("On Your Mark")
+                ).withProfileImagesClickable(false)
+                .withOnlyMainProfileImageVisible(true)
+                .withPaddingBelowHeader(false)
+                .withHeaderBackgroundScaleType(ImageView.ScaleType.CENTER)
+                .withCompactStyle(false)
+                .build();
+
+        drawer = new DrawerBuilder()
+                .withActivity(activity)
+                .withAccountHeader(headerResult)
+                .withActionBarDrawerToggleAnimated(true)
+                .withCloseOnClick(true)
+                .withHeaderPadding(true)
+                .withToolbar((Toolbar) findViewById(R.id.toolbar))
+                .addDrawerItems(
+                        item1,
+                        item2,
+                        item3
+                ).addStickyDrawerItems(
+                        item4
+                ).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Intent intent;
+                        switch ((int) drawerItem.getIdentifier()) {
+                            case 1:
+                                drawer.closeDrawer();
+                                break;
+                            case 2:
+                                intent = new Intent(MainActivity.this, SelectImageActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 3:
+                                Toast.makeText(activity, "Settings", Toast.LENGTH_SHORT).show();
+                                intent = new Intent(MainActivity.this, SelectImageActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 10:
+                                Toast.makeText(activity, "Yee", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                Toast.makeText(activity, "You haven't configured an onClick event for this button", Toast.LENGTH_SHORT).show();
+                        }
+                        return false;
+                    }
+                })
+                .build();
     }
 }
