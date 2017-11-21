c4929e4df41232ac258873178a0aeb3168a214bb
Sohit Pal <sohitpal@outlook.com>
2017-11-20 19:02:09 -0500

    Added Monthly summary box on main activity

--- app/OnYourMark/app/src/main/java/com/example/mikezurawski/onyourmark/views/MainActivity.java
+++ app/OnYourMark/app/src/main/java/com/example/mikezurawski/onyourmark/views/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mikezurawski.onyourmark.views;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,12 @@ import com.example.mikezurawski.onyourmark.R;
 
 import com.example.mikezurawski.onyourmark.database.BudgetItem;
 import com.example.mikezurawski.onyourmark.database.DatabaseHandler;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -23,7 +30,9 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 // Keys
 
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initHamburgerMenu(this);
+        initMonthlySummary();
 
         // ?
         database = new DatabaseHandler(this);
@@ -53,40 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         database.addBudgetItem(budgetItem);
 
-        Button viewBudgetsBtn = (Button) findViewById(R.id.view_budget_btn);
-        viewBudgetsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                displayAllData();
-            }
-        });
-
-        Button viewLeftoversBtn = (Button) findViewById(R.id.view_leftovers_btn);
-        viewLeftoversBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MonthlyActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button viewBreakdownBtn = (Button) findViewById(R.id.view_breakdown_btn);
-        viewBreakdownBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BreakdownActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button viewAddItemBtn = (Button) findViewById(R.id.add_budget_item_btn);
-        viewAddItemBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
-                startActivity(intent);
-            }
-        });
+        displayAllData();
     }
 
     // TODO: Remove -- Debugging only
@@ -100,6 +77,36 @@ public class MainActivity extends AppCompatActivity {
                     + " |\n";
         }
         System.out.println(toPrint);
+    }
+
+    private void initMonthlySummary() {
+
+        PieChart pieChart = (PieChart) findViewById(R.id.chart);
+
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(60.5f, ""));
+        entries.add(new PieEntry(39.5f, ""));
+
+        final int[] MY_COLORS = {Color.BLUE, Color.RED};
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for(int c: MY_COLORS) colors.add(c);
+
+        PieDataSet set = new PieDataSet(entries, "Monthly Money Leftovers");
+        set.setColors(colors);
+        set.setDrawValues(false);
+        PieData data = new PieData(set);
+
+        Legend legend = pieChart.getLegend();
+        legend.setEnabled(false);
+        Description description = new Description();
+        description.setText("");
+        pieChart.setDescription(description);
+        pieChart.setData(data);
+        pieChart.animateY(500);
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.invalidate(); // refresh
     }
 
     private void initHamburgerMenu(final Activity activity) {
@@ -136,9 +143,9 @@ public class MainActivity extends AppCompatActivity {
         // Create the AccountHeader
         AccountHeader headerResult = new AccountHeaderBuilder()
                 .withActivity(activity)
-                .withHeaderBackground(R.color.md_black_1000)
+                .withHeaderBackground(R.color.colorPrimaryDark)
                 .addProfiles(new ProfileDrawerItem()
-                        .withIcon(R.mipmap.ic_launcher)
+                        .withIcon(R.mipmap.ic_launcher_round)
                         .withName("On Your Mark")
                 ).withProfileImagesClickable(false)
                 .withOnlyMainProfileImageVisible(true)
@@ -147,13 +154,15 @@ public class MainActivity extends AppCompatActivity {
                 .withCompactStyle(false)
                 .build();
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("On Your Mark");
+
         drawer = new DrawerBuilder()
                 .withActivity(activity)
                 .withAccountHeader(headerResult)
-                .withActionBarDrawerToggleAnimated(true)
                 .withCloseOnClick(true)
                 .withHeaderPadding(true)
-                .withToolbar((Toolbar) findViewById(R.id.toolbar))
+                .withToolbar(toolbar)
                 .addDrawerItems(
                         item1,
                         item2,
@@ -174,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             case 3:
                                 Toast.makeText(activity, "Settings", Toast.LENGTH_SHORT).show();
-                                intent = new Intent(MainActivity.this, SelectImageActivity.class);
+                                intent = new Intent(MainActivity.this, BreakdownActivity.class);
                                 startActivity(intent);
                                 break;
                             case 10:
